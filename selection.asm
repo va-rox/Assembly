55ed123c2d82b52540f1a3aef8e876bd50d4cbc7
va-rox <97227132+va-rox@users.noreply.github.com>
2024-02-22 15:20:48 +0530

    Create selection.asm

---
+++ selection.asm
@@ -0,0 +1,41 @@
+.model small
+.data
+    num dw 05h, 04h, 03h, 02h, 01h
+    count dw 5h
+
+.code
+    mov ax, @data
+    mov ds, ax
+
+    mov cx, count
+    dec cx
+    mov si, 2
+
+SortLoop:
+    push si
+InnerLoop:
+    mov ax, num[si - 2]
+    mov bx, num[si]
+    cmp ax, bx
+    jnb NextIteration
+
+    mov num[si - 2], bx
+    mov num[si], ax
+    dec si
+    dec si
+    jnz InnerLoop
+
+NextIteration:
+    dec cx
+    jz ExitProgram
+
+    pop si
+    inc si
+    inc si
+    jmp SortLoop
+
+ExitProgram:
+    mov ah, 4ch
+    int 21h
+
+end
